a1ee27284868b2497f880194f2c7465484e6d829
milidiwan <milianudiwan@gmail.com>
2021-05-16 23:17:26 +1000

    Add employee class

---
+++ src/Employee.java
@@ -0,0 +1,28 @@
+
+public class Employee {
+
+    String name;
+    double salary;
+
+    public double getSalary()
+    {
+        return this.salary;
+    }
+
+    public  String  getName()
+    {
+        return this.name;
+    }
+
+    public void setName(String empName)
+    {
+        this.name = empName;
+    }
+
+    public void setSalary(double empSalary)
+    {
+        this.salary = empSalary;
+    }
+
+}
+
